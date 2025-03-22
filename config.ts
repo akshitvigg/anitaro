9ce478369abf20e0ddfd533086223b51129d4d43
Akshit Vig <akshitvig48@gmail.com>
2025-03-22 20:26:01 +0530

    anime section alive

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 export const CONSUMET_URL =
-  "https://consumet-api-9p6q.onrender.com/anime/gogoanime";
-export const MANGA_URL = "https://consumet-api-9p6q.onrender.com/manga";
+  "https://consumet-api-4m21.onrender.com/anime/animepahe";
+export const MANGA_URL = "https://consumet-api-4m21.onrender.com/manga";
