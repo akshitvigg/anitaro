8f0aa712736eb1d56200dd07ddf6a363ea1f79c3
Akshit Vig <akshitvig48@gmail.com>
2025-03-23 13:12:46 +0530

    api changed

--- config.ts
+++ config.ts
@@ -1,3 +1,2 @@
-export const CONSUMET_URL =
-  "https://consumet-api-4m21.onrender.com/anime/animepahe";
+export const CONSUMET_URL = "https://consumet-api-4m21.onrender.com/anime/zoro";
 export const MANGA_URL = "https://consumet-api-4m21.onrender.com/manga";
