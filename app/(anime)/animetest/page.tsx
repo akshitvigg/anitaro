f272648997be4aff47abf170fac3b1dbc349cfed
Akshit Vig <akshitvig48@gmail.com>
2025-03-23 16:31:43 +0530

    d

--- app/(anime)/animetest/page.tsx
+++ app/(anime)/animetest/page.tsx
@@ -2,7 +2,7 @@ import VideoPlayer from "@/components/videoplayer";
 
 export default function Animetest() {
   const m3u8ProxyUrl =
-    "https://lightningspark77.pro/_v7/9c91e62b08efed2c227af0fd7162703c2d8ca95ecfe25bcda8cd02b8ca65c3bda9433b8f6439c28ef8ee52366062ce838407eb90be2cc46f8bbfee956c689a8ec476d6580918f4af9b7575a3fa38f2ed5501ecd242ce6aa5aa3678fc8b5d1f382185630706cf9c83962f4786b18b9392/master.m3u8";
+    "https://m3u8proxy-dsu3.onrender.com/m3u8-proxy?url=https://eh.netmagcdn.com:2228/hls-playback/462f0ad7b60573e9fceb9391a56938ef1a2f5b224bf5ecc273c50df25aa3619f364e68a109158ef83abf922fbe6416e074fda870f63afcae80f54f92afb10fa517fe87278e7763a42f13cdcc4286c75f0c9c59fb206491f4356a118f02a0e3856a39bef1a39c7f0cda8de2f5a9bd424fea54d2b5c87ea3c0e367fafb248cfbbffb9e3f793526007ae2a4e84fa1f4bc25/master.m3u8";
 
   return (
     <div>
