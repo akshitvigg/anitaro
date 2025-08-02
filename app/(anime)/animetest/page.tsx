320f42f1b772fa2ee3ca0414e6533487fe261a3c
akshitvigg <akshitvig48@gmail.com>
2025-08-02 17:39:39 +0530

    fixed ep

--- app/(anime)/animetest/page.tsx
+++ app/(anime)/animetest/page.tsx
@@ -4,11 +4,11 @@ const Play = () => {
   return (
     <div>
       <M3U8Player
-        m3u8Url="https://cdn.dotstream.buzz/anime/eccbc87e4b5ce2fe28308fd9f2a7baf3/8f4a8ca728cd7a9ac1d2853d1c1c3d9b/master.m3u8"
+        m3u8Url="https://cdn.dotstream.buzz/anime/a1d0c6e83f027327d8461063f4ac58a6/8b92f1aa1467d68404a9cca6a4895a32/master.m3u8"
         referer="https://megaplay.buzz/stream/s-2/steinsgate-3?ep=230/dub"
         subtitles={[
           {
-            url: "https://cdn.dotstream.buzz/anime/eccbc87e4b5ce2fe28308fd9f2a7baf3/8f4a8ca728cd7a9ac1d2853d1c1c3d9b/subtitles/eng-0.vtt",
+            url: "https://cdn.dotstream.buzz/anime/a1d0c6e83f027327d8461063f4ac58a6/8b92f1aa1467d68404a9cca6a4895a32/subtitles/eng-0.vtt",
             lang: "English",
           },
           {
