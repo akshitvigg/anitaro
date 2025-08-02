7ead630b1a17cfaca595d2bf9e47247f2ce2d641
akshitvigg <akshitvig48@gmail.com>
2025-08-03 02:29:19 +0530

    api updated

--- config.ts
+++ config.ts
@@ -1,2 +1,3 @@
-export const CONSUMET_URL = "https://consumet-api-4m21.onrender.com/anime/zoro";
+export const CONSUMET_URL =
+  "https://aniwatch-api-production-b308.up.railway.app/api/v2/hianime";
 export const MANGA_URL = "https://consumet-api-4m21.onrender.com/manga";
