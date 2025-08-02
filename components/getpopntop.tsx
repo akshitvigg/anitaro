3fcd65c3cafcf9ad839be198209750951186c999
akshitvigg <akshitvig48@gmail.com>
2025-08-03 02:31:38 +0530

    getpopntop repairred

--- components/getpopntop.tsx
+++ components/getpopntop.tsx
@@ -1,16 +1,19 @@
 "use client";
-import { CONSUMET_URL } from "@/config";
 import { Button, Image } from "@nextui-org/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Spinner } from "@nextui-org/react";
 import Link from "next/link";
+import { unique } from "next/dist/build/utils";
 
 interface Anime {
   id: string;
-  title: string;
-  image: string;
-  episodes: number;
+  name: string;
+  poster: string;
+  episodes?: {
+    sub: number;
+    dub: number | null;
+  };
 }
 
 export const HomePageAnimes = () => {
@@ -20,14 +23,44 @@ export const HomePageAnimes = () => {
   const [btnLoading, setbtnLoading] = useState<boolean>(false);
   const [animeId, setAnimeid] = useState("");
 
-  const getTrendinganime = async (path: string) => {
+  const getTrendinganime = async (_path: string) => {
     try {
       setbtnLoading(true);
-      const response = await axios.get<{ results: Anime[] }>(
-        CONSUMET_URL + path
-      );
-      setTopanime(response.data.results);
-      setActiveBtn(path);
+      const response = await axios.get("/api/home");
+      //@ts-ignore
+      const data = response.data.data;
+      console.log(data);
+
+      let animeGroup: Anime[] = [];
+
+      if (_path === "/top-airing") {
+        const combined = [
+          ...data.spotlightAnimes,
+          ...data.trendingAnimes,
+          ...data.topAiringAnimes,
+        ];
+        animeGroup = removeDuplicateAnimes(combined);
+      } else if (_path === "/most-popular") {
+        const combined = [
+          ...data.mostPopularAnimes,
+          ...data.mostFavoriteAnimes,
+          ...data.top10Animes.today,
+          ...data.top10Animes.week,
+          ...data.top10Animes.month,
+        ];
+
+        animeGroup = removeDuplicateAnimes(combined);
+      } else if (_path === "/recent-episodes") {
+        const combined = [
+          ...data.latestEpisodeAnimes,
+          ...data.latestCompletedAnimes,
+          ...data.topUpcomingAnimes,
+        ];
+        animeGroup = removeDuplicateAnimes(combined);
+      }
+
+      setTopanime(animeGroup);
+      setActiveBtn(_path);
     } catch (e) {
       console.log(e);
     } finally {
@@ -35,10 +68,20 @@ export const HomePageAnimes = () => {
     }
   };
 
+  function removeDuplicateAnimes(animes: Anime[]) {
+    const uniqueMap = new Map();
+
+    for (const anime of animes) {
+      if (!uniqueMap.has(anime.id)) {
+        uniqueMap.set(anime.id, anime);
+      }
+    }
+    return Array.from(uniqueMap.values());
+  }
+
   useEffect(() => {
     setLoading(true);
-    getTrendinganime("/top-airing");
-    setLoading(false);
+    getTrendinganime("/top-airing").then(() => setLoading(false));
   }, []);
 
   const getProxyImageUrl = (originalUrl: string) => {
@@ -46,7 +89,7 @@ export const HomePageAnimes = () => {
   };
 
   return (
-    <div className=" min-h-screen mt-32  -translate-y-14 bg-[#0a0a0a]">
+    <div className="min-h-screen mt-32 -translate-y-14 bg-[#0a0a0a]">
       {loading ? (
         <div className="flex justify-center">
           <Spinner size="lg" />
@@ -92,38 +135,38 @@ export const HomePageAnimes = () => {
           </div>
 
           {btnLoading ? (
-            <div className=" flex justify-center mt-10">
+            <div className="flex justify-center mt-10">
               <Spinner size="lg" />
             </div>
           ) : (
-            <div className=" flex justify-center">
-              <div className=" sm:ml-[80px] ml-4 mr-4 sm:mr-[80px]   mt-16  grid gap-x-2 gap-y-4 sm:grid-cols-3">
+            <div className="flex justify-center">
+              <div className="sm:ml-[80px] ml-4 mr-4 sm:mr-[80px] mt-16 grid gap-x-2 gap-y-4 sm:grid-cols-3">
                 {topanime.map((anime) => (
-                  <div className=" flex  col-span-1 " key={anime.id}>
+                  <div className="flex col-span-1" key={anime.id}>
                     <Link href={`/animeinfo/${anime.id}`}>
                       <div>
                         <Image
-                          // isBlurred={true}
                           isZoomed
                           onClick={() => {
                             setAnimeid(anime.id);
                           }}
-                          className=" hover:cursor-pointer mb-2 border border-[#3f3f46] object-cover"
+                          className="hover:cursor-pointer mb-2 border border-[#3f3f46] object-cover"
                           loading="lazy"
                           width={440}
                           height={240}
                           radius="sm"
-                          alt="anime images"
-                          src={getProxyImageUrl(anime.image)}
+                          alt="anime image"
+                          src={getProxyImageUrl(anime.poster)}
                         />
                         <div className="mt-2">
-                          {anime.title.length > 60
-                            ? anime.title.slice(0, 17) + "..."
-                            : anime.title}
+                          {anime.name.length > 60
+                            ? anime.name.slice(0, 17) + "..."
+                            : anime.name}
                         </div>
-                        <div className=" text-gray-400">
-                          {activeButton === "/popular" ? null : "Episode "}
-                          {anime.episodes}
+                        <div className="text-gray-400">
+                          {anime.episodes?.sub
+                            ? `Episode ${anime.episodes.sub}`
+                            : ""}
                         </div>
                       </div>
                     </Link>
