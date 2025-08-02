f4a47aa44638b13aaaf60f6a27569be9dfd92861
akshitvigg <akshitvig48@gmail.com>
2025-08-02 17:10:19 +0530

    proxy works

--- components/videoplayer.tsx
+++ components/videoplayer.tsx
@@ -1,24 +1,36 @@
 "use client";
-
-import { useEffect, useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
 
-//@ts-ignore
-const VideoPlayer = ({ m3u8Url }) => {
+type M3U8PlayerProps = {
+  m3u8Url: string;
+  referer: string;
+  subtitles?: { url: string; lang: string }[];
+};
+
+export const M3U8Player: React.FC<M3U8PlayerProps> = ({
+  m3u8Url,
+  referer,
+  subtitles = [],
+}) => {
   const videoRef = useRef(null);
-  const playerRef = useRef(null);
+  const playerRef = useRef<any>(null);
+
+  const proxyUrl = `https://rust-proxy-m3u8.onrender.com/?url=${encodeURIComponent(
+    m3u8Url
+  )}&headers=${encodeURIComponent(JSON.stringify({ Referer: referer }))}`;
 
   useEffect(() => {
-    if (!playerRef.current) {
-      //@ts-ignore
+    if (!playerRef.current && videoRef.current) {
       playerRef.current = videojs(videoRef.current, {
         controls: true,
         autoplay: false,
+        fluid: true,
         preload: "auto",
         sources: [
           {
-            src: m3u8Url,
+            src: proxyUrl,
             type: "application/x-mpegURL",
           },
         ],
@@ -27,23 +39,32 @@ const VideoPlayer = ({ m3u8Url }) => {
 
     return () => {
       if (playerRef.current) {
-        //@ts-ignore
         playerRef.current.dispose();
         playerRef.current = null;
       }
     };
-  }, [m3u8Url]);
+  }, [proxyUrl]);
 
   return (
-    <div>
+    <div data-vjs-player>
       <video
         ref={videoRef}
-        className="video-js vjs-default-skin"
-        width="640"
-        height="360"
-      ></video>
+        className="video-js vjs-default-skin w-full h-auto rounded-lg"
+        crossOrigin="anonymous"
+      >
+        {subtitles.map((track, index) => (
+          <track
+            key={index}
+            src={track.url}
+            label={track.lang}
+            kind="subtitles"
+            srcLang={track.lang.slice(0, 2).toLowerCase()}
+            default={track.lang === "English"}
+          />
+        ))}
+      </video>
     </div>
   );
 };
 
-export default VideoPlayer;
+export default M3U8Player;
